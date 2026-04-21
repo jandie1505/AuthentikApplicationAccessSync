bf4706f55c797c9b698c484a0f7802983b2951df
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2026-04-21 12:53:30 +0200

    removed invisible try catch blocks in UserUtilities.cs

--- jellyfin/AuthentikJellyfinSync/Sync/UserUtilities.cs
+++ jellyfin/AuthentikJellyfinSync/Sync/UserUtilities.cs
@@ -7,42 +7,28 @@ public class UserUtilities
 {
     public static Authentik.Client.Model.User? GetUser(CoreApi api, UserIdentifierType userIdentifierType, string oauthId)
     {
-        try
+        switch (userIdentifierType)
         {
-            switch (userIdentifierType)
-            {
-                case UserIdentifierType.Pk:
-                    return api.CoreUsersRetrieve(int.Parse(oauthId));
-                case UserIdentifierType.Uuid:
-                    return GetUserByUuid(api, oauthId);
-                case UserIdentifierType.Email:
-                    return GetUserByEmail(api, oauthId);
-                case UserIdentifierType.Username:
-                    return GetUserByUsername(api, oauthId);
-                default:
-                    return null;
-            }
-        }
-        catch (FormatException exception)
-        {
-            return null;
+            case UserIdentifierType.Pk:
+                return api.CoreUsersRetrieve(int.Parse(oauthId));
+            case UserIdentifierType.Uuid:
+                return GetUserByUuid(api, oauthId);
+            case UserIdentifierType.Email:
+                return GetUserByEmail(api, oauthId);
+            case UserIdentifierType.Username:
+                return GetUserByUsername(api, oauthId);
+            default:
+                return null;
         }
     }
 
     private static Authentik.Client.Model.User? GetUserByUuid(CoreApi api, string uuid)
     {
-        try
-        {
-            var users = api.CoreUsersList(uuid: Guid.Parse(uuid));
-            if (users.Results.Count != 1)
-                return null;
-            
-            return users.Results[0];
-        }
-        catch (FormatException exception)
-        {
+        var users = api.CoreUsersList(uuid: Guid.Parse(uuid));
+        if (users.Results.Count != 1)
             return null;
-        }
+            
+        return users.Results[0];
     }
     
     private static Authentik.Client.Model.User? GetUserByUsername(CoreApi api, string username)
